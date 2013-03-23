1b88defd9e0c583de932fdd88ef69f9e4db5599b
Vlad <vladigoal@dad.pedrito>
2013-03-23 11:46:18 +0200

    ticket:3 create poster and interactive stars

--- movies/static/js/base.js
+++ movies/static/js/base.js
@@ -3,5 +3,20 @@
  */
 
 
-$(function () {
+$(function(){
+    
+    $(".stars li").hover(function(){
+        var num =  $(this).index()
+        active_stars(num)
+    }, function(){
+        $(".stars li").removeClass("hover")
+    })
+
+
+    function active_stars(num){
+        for (var i=0; i<=num; i++){
+            $(".stars li").eq(i).addClass("hover")
+        }
+    }
+
 });
